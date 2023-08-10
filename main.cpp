bef99e47af6ba12f120fc5096e29c09fd8cfd386
garyc89 <138969820+garyc89@users.noreply.github.com>
2023-08-10 11:36:01 +0100

    Labs Q1 part 1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+// Error: If the `button_pressed` variable isn't declared as `volatile`, the compiler might optimize 
+// it away, causing the main loop to not notice changes made inside the ISR.
+// Solution: Declare the flag as `volatile` to ensure it isn't optimized away and its changes are 
+// immediately visible across different contexts.
 
-    }
+InterruptIn button(USER_BUTTON);
+volatile bool button_pressed = false;  // Declare flag as volatile
+
+void button_isr() {
+    button_pressed = true;
 }
 
+int main() {
+    button.fall(&button_isr);
+
+    while (1) {
+        if (button_pressed) {
+            printf("Button pressed\r\n");
+            button_pressed = false;  // Reset the flag
+        }
+        ThisThread::sleep_for(10ms);
+    }
+}
